2b2f764b5e388f51a14258168ac3d47caf702055
Bao Nguyen <bao.nn193989@sis.hust.edu.vn>
2024-10-16 19:46:09 +0800

    Update sweep_sequential.sh

--- validation/sweeping/sweep_sequential.sh
+++ validation/sweeping/sweep_sequential.sh
@@ -6,7 +6,7 @@ INFO=ft:davinci-002:ethicalytics:informative:A0WuCDTp
 LOG_FILE="sweep_output.log"
 EVAL_DATASET="truthful_qa"
 TRAIN_DATASET="truthful_qa"
-CACHE="/home/users/nus/binhnt/scratch/.cache/huggingface/hub"
+CACHE=""
 # HF_NAMES = {llama_7B'
 # 'alpaca_7B', 'vicuna_7B', 'llama2_chat_13B', 'llama3_8B'
 # TEST
