f53ef375df73723a5b0edfea604358f5194d9c5f
Bao Nguyen <bao.nn193989@sis.hust.edu.vn>
2024-10-16 19:46:47 +0800

    Update get_activations.sh

--- get_activations/get_activations.sh
+++ get_activations/get_activations.sh
@@ -2,7 +2,7 @@
 # 'alpaca_7B', 'vicuna_7B', 'llama2_chat_13B', 'llama3_8B'
 # TEST
 
-CACHE="/home/baocmt/.cache/huggingface/hub"
+CACHE=""
 for MODEL in llama_7B alpaca_7B vicuna_7B llama2_chat_13B llama3_8B; do
     CUDA_VISIBLE_DEVICES=0 python get_activations.py --model_name $MODEL --cache_dir $CACHE --dataset_name tqa_mc2
     CUDA_VISIBLE_DEVICES=0 python get_activations.py --model_name $MODEL --cache_dir $CACHE --dataset_name tqa_gen_end_q
